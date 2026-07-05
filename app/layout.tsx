acc8e2165e47902bc876bf0aca2245ecc2684284
csharjeel05-source <csharjeel360@gmil.com>
2026-07-05 16:30:11 +0500

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -200,6 +200,7 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        <meta name="google-site-verification" content="lzAJ81Xob8zLqzbn-Kn5yWpG2M9zyD1yR8LJOpWDZkE" />
         <ClientRoutePrefetch />
         <Header />
         <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
